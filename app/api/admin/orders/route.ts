10f2d3425ba4955e9d1addfb58dc1c6e63d139ac
Jeremy Johnatan <jeremy.johnatan@gmail.com>
2025-12-01 22:06:14 +0700

    fix error deploy vercel

--- app/api/admin/orders/route.ts
+++ app/api/admin/orders/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 import { getServerSession } from "next-auth"; 
-import { authOptions } from "../auth/[...nextauth]/route"; 
+import { authOptions } from "@/lib/auth";
 import { beamsClient } from "@/lib/beams"; 
 import { Xendit } from 'xendit-node'; // 1. Import Xendit
 
